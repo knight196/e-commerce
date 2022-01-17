7e30308d3cc45abb418dd10b6d362fc001fee5d1
knight196 <71406824+knight196@users.noreply.github.com>
2022-01-17 15:18:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ fetch('data.json')
             buttonClicked.parentElement.parentElement.remove();
             var countless = document.querySelector('.count').innerHTML = --count;
             if(countless ==  0){
-               location.href="phoneshop.html";
+               location.href="index.html";
             }
             updateCartTotal();
          }
